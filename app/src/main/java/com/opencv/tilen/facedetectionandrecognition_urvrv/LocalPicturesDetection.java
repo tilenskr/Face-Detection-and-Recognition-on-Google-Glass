97902f97faff21e480632992dbbdc821266f02a4
tilenskr <skrinjar.tilen@gmail.com>
2015-06-12 01:07:15 +0200

    Created local picture loading and showing (from drawables currently)

---
+++ app/src/main/java/com/opencv/tilen/facedetectionandrecognition_urvrv/LocalPicturesDetection.java
@@ -0,0 +1,42 @@
+package com.opencv.tilen.facedetectionandrecognition_urvrv;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+
+/**
+ * Created by Tilen on 10.6.2015.
+ */
+public class LocalPicturesDetection {
+    private Mat localPicture;
+    private Context mContext;
+    public LocalPicturesDetection(Context context,int resourceId) throws IOException {
+        this.mContext = context;
+       /* localPicture = new Mat();
+        Bitmap bMap= BitmapFactory.decodeResource(mContext.getResources(), R.drawable.lena);
+        Utils.bitmapToMat(bMap, localPicture);*/
+
+        localPicture = Utils.loadResource(context, resourceId);
+        Global.TestDebug("LocalPicturesDetection.LocalPicturesDetection: localPicture " + localPicture.cols());
+
+
+    }
+
+    public Mat getlocalPicture() {
+        return localPicture;
+    }
+    public static Bitmap matToBitmap(Mat inputPicture)
+    {
+        Mat convertedPicture = new Mat();
+        Global.TestDebug("test : " +inputPicture.cols());
+        Imgproc.cvtColor(inputPicture, convertedPicture, Imgproc.COLOR_RGB2BGRA);
+        Bitmap bitmapPicture = Bitmap.createBitmap(inputPicture.cols(), inputPicture.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(convertedPicture, bitmapPicture);
+        return bitmapPicture;
+    }
+}
