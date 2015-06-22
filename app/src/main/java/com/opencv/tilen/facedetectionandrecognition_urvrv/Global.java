52fe70e99f8e591c12ee7bec99b169b13a2e5549
tilenskr <skrinjar.tilen@gmail.com>
2015-06-23 00:02:50 +0200

    Implemented extended logging.

--- app/src/main/java/com/opencv/tilen/facedetectionandrecognition_urvrv/Global.java
+++ app/src/main/java/com/opencv/tilen/facedetectionandrecognition_urvrv/Global.java
@@ -10,14 +10,29 @@ public class Global {
     private static final String INFOTAG = "OCVSeminarska.info";
     private static final String ERRORTAG = "OCVSeminarska.error";
     private static final String TESTTAG = "OCVSeminarska.test";
-    public static void LogDebug(String message)
-    {
-        Log.d(DEBUGTAG, message);
+
+    private static final boolean debug = true;
+    private static final boolean info = true;
+    private static final boolean error = true;
+    private static final boolean test = true;
+
+    public static void LogDebug(String message) {
+        if (debug)
+            Log.d(DEBUGTAG, message);
     }
-    public static void InfoDebug(String message)
-    {
-        Log.i(INFOTAG, message);
+
+    public static void InfoDebug(String message) {
+        if (info)
+            Log.i(INFOTAG, message);
     }
-    public static void ErrorDebug(String message) {Log.e(ERRORTAG,message);}
-    public static void TestDebug(String message) {Log.d(TESTTAG, message);}
+
+    public static void ErrorDebug(String message) {
+        if (error)
+            Log.e(ERRORTAG, message);
+    }
+
+    public static void TestDebug(String message) {
+        if (test)
+            Log.d(TESTTAG, message);
+    }
 }
